b62f95b5c25bf73979d4f61f6be3434eb9fb7ba5
Adegbenga Adeye <adegbenga.adeye@outlook.com>
2020-03-15 15:14:44 +0100

    reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +9,13 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let contents = read_file(filename);
+
+    println!("With text:\n{}", contents);
+}
+
+fn read_file(filename: &std::string::String) -> std::string::String {
+    fs::read_to_string(filename)
+        .expect("Something went wrong reading the file")
 }
