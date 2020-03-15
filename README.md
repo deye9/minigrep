386dff01b40533bc68b41e3398d7b6432b360b31
Adegbenga Adeye <adegbenga.adeye@outlook.com>
2020-03-15 20:29:38 +0100

    Added Environment Variables

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # minigrep
 
 grep (globally search a regular expression and print)
+
+# Commands that can be used
+
+1. cargo run to poem.txt
+2. CASE_INSENSITIVE=1 cargo run to poem.txt
+3. cargo test
+4. cargo run frog poem.txt
+5. cargo run body poem.txt
+6. cargo run monomorphization poem.txt
--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,14 @@
 
 use std::error::Error;
 use std::fs;
+use std::env;
 
 #[derive(Debug)]
 #[derive(PartialEq)]
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -19,14 +21,22 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -45,23 +55,47 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
 
-    #[test]
-    fn one_result() {
-        let query = "duct";
-        let contents = "\
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+#[test]
+fn case_sensitive() {
+    let query = "duct";
+    let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
-        assert_eq!(
-            vec!["safe, fast, productive."],
-            search(query, contents)
-        );
-    }
+    assert_eq!(
+        vec!["safe, fast, productive."],
+        search(query, contents)
+    );
+}
+
+#[test]
+fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+    assert_eq!(
+        vec!["Rust:", "Trust me."],
+        search_case_insensitive(query, contents)
+    );
 }
 
 #[test]
@@ -70,6 +104,7 @@ fn struct_new_fails_when_arguments_are_not_enough() {
     let res: Vec<String> = my_string.split("something").map(|s| s.to_string()).collect();
 
     let config = Config::new(&res);
+    assert_eq!(config.is_err(), true);
     assert_eq!(config.err(), Some("not enough arguments"));
 }
 
